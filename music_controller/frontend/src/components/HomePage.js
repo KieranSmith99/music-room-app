989b76d946fa3d2dd3576a1baa7c8287867a5ac2
Kieran Smith <kieransmith63@gmail.com>
2021-01-09 12:09:46 +0000

    Finished main project
    Project is now functional, added info pages using functional rather than class based components

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -4,6 +4,7 @@ import CreateRoomPage from './CreateRoomPage';
 import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 import Room from "./Room"
+import Info from "./Info"
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -39,6 +40,10 @@ export default class HomePage extends Component {
                             Join a Room
                         </Button>
 
+                        <Button color="default" to='/info' component={Link}>
+                            Info
+                        </Button>
+
                         <Button color="secondary" to='/create' component={Link}>
                             Create a Room
                         </Button>
@@ -67,6 +72,7 @@ export default class HomePage extends Component {
                     </Route>
 
                     <Route path="/join" component={RoomJoinPage} />
+                    <Route path="/info" component={Info} />
                     <Route path="/create" component={CreateRoomPage} />
                     <Route path="/room/:roomCode" 
                         render={(props) => {
--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -71,7 +71,6 @@ export default class Room extends Component {
           })
           .then((data) => {
             this.setState({ song: data });
-            console.log(data);
           });
     }
 
